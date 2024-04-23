e1c57c317b86ec50992c53b20462fe1b83441f72
Pedro Rocha <pedrojrocha@outlook.com>
2024-04-23 13:06:02 +0200

    Publish method should receive Message as input

--- messaging/message/message.go
+++ messaging/message/message.go
@@ -4,7 +4,7 @@ import "context"
 
 // Interface for messaging systems
 type MessagingSystem interface {
-	Publish(ctx context.Context, data interface{}) error
+	Publish(ctx context.Context, data Message) error
 	Subscribe(ctx context.Context) (<-chan Message, context.CancelFunc, error)
 	Close(ctx context.Context) error
 }
