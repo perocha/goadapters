e38f448667f378e1b369204b7c320499981a7594
Pedro Rocha <pedrojrocha@outlook.com>
2024-04-23 11:06:11 +0200

    Add a function to return a key-value map of Message

--- messaging/messaging.go
+++ messaging/messaging.go
@@ -2,6 +2,8 @@ package messaging
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 )
 
 // EventWithOperationID struct that contains an operation ID and an Event
@@ -11,9 +13,30 @@ type Message struct {
 	Data        interface{}
 }
 
+// ToMap tries to convert the Data field of the Message to a map[string]interface{}
+func (m *Message) ToMap() (map[string]interface{}, error) {
+	if m.Data == nil {
+		return nil, errors.New("message::ToMap::Data is nil")
+	}
+
+	// Convert the Data field to a map[string]interface{}
+	dataBytes, ok := m.Data.([]byte)
+	if !ok {
+		return nil, errors.New("message::ToMap::Error Data is not a valid JSON")
+	}
+
+	// Unmarshal the data to a map[string]interface{}
+	var result map[string]interface{}
+	if err := json.Unmarshal(dataBytes, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// Interface for messaging systems
 type MessagingSystem interface {
 	Publish(ctx context.Context, data interface{}) error
-	// TODO how to deal with "topic" concept?
 	Subscribe(ctx context.Context) (<-chan Message, context.CancelFunc, error)
 	Close(ctx context.Context) error
 }
