d26cd93d779f61c101a5d0e990d085ee5741e2c6
Pedro Rocha <pedrojrocha@outlook.com>
2024-05-01 14:23:19 +0200

    Additional test coverage

--- messaging/httpadapter/httpadapter_test.go
+++ messaging/httpadapter/httpadapter_test.go
@@ -3,9 +3,13 @@ package httpadapter_test
 import (
 	"context"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/perocha/goadapters/messaging/httpadapter"
+	"github.com/perocha/goadapters/messaging/message"
 	"github.com/perocha/goutils/pkg/telemetry"
 	"github.com/stretchr/testify/assert"
 )
@@ -49,4 +53,28 @@ func TestPublisherInitializer(t *testing.T) {
 	assert.IsType(t, &httpadapter.HTTPAdapterImpl{}, adapter)
 	assert.Equal(t, endPointURL, adapter.GetEndPoint())
 	assert.Equal(t, portNumber, adapter.GetPortNumber())
+}
+
+func TestPublish(t *testing.T) {
+	// Create a mock HTTP server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	ctx := initializeTelemetry()
+	endPointURL := "http://localhost"
+	portNumber := strings.Split(server.URL, ":")[2]
+
+	// Set the URL of the mock server
+	server.URL = endPointURL + ":" + portNumber
+
+	// Initialize the HTTP adapter
+	adapter, err := httpadapter.PublisherInitializer(ctx, endPointURL, portNumber)
+	assert.NoError(t, err)
+
+	// Build mock Message
+	msg := message.NewMessage("1234", nil, "success", "test", []byte("test"))
+	err = adapter.Publish(ctx, msg)
+	assert.NoError(t, err)
 }
