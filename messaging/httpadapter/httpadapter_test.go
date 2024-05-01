37853473481763567804e76334e81d08e4f71fb7
Pedro Rocha <pedrojrocha@outlook.com>
2024-05-01 13:42:13 +0200

    Add mock telemetry to tests

--- messaging/httpadapter/httpadapter_test.go
+++ messaging/httpadapter/httpadapter_test.go
@@ -2,14 +2,29 @@ package httpadapter_test
 
 import (
 	"context"
+	"log"
 	"testing"
 
 	"github.com/perocha/goadapters/messaging/httpadapter"
+	"github.com/perocha/goutils/pkg/telemetry"
 	"github.com/stretchr/testify/assert"
 )
 
+func initializeTelemetry() context.Context {
+	// Initialize telemetry package
+	serviceName := "httpadapter"
+	telemetryConfig := telemetry.NewXTelemetryConfig("", serviceName, "info", 1)
+	xTelemetry, err := telemetry.NewXTelemetry(telemetryConfig)
+	if err != nil {
+		log.Fatalf("Main::Fatal error::Failed to initialize XTelemetry %s\n", err.Error())
+	}
+	// Add telemetry object to the context, so that it can be reused across the application
+	ctx := context.WithValue(context.Background(), telemetry.TelemetryContextKey, xTelemetry)
+	return ctx
+}
+
 func TestConsumerInitializer(t *testing.T) {
-	ctx := context.Background()
+	ctx := initializeTelemetry()
 	endPointURL := "http://localhost"
 	portNumber := "8080"
 
@@ -23,7 +38,7 @@ func TestConsumerInitializer(t *testing.T) {
 }
 
 func TestPublisherInitializer(t *testing.T) {
-	ctx := context.Background()
+	ctx := initializeTelemetry()
 	endPointURL := "http://localhost"
 	portNumber := "8080"
 
